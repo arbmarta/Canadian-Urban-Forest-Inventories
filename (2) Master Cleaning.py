c5c24b0c2d82ef641b647714fd40dff0aa75c3ff
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2024-09-21 17:35:03 -0500

    Update (2) Master Cleaning.py

--- (2) Master Cleaning.py
+++ (2) Master Cleaning.py
@@ -1,90 +1,50 @@
 import pandas as pd
+import re
 
 # Load the merged CSV file
-merged_df = pd.read_csv(r'C:\Users\alexj\Documents\Research\Canadian Urban Forest Inventories - Structure and Diversity\Python Scripts and Datasets\Master Dataset.csv', low_memory=False)
+merged_df = pd.read_csv(r'C:\Users\alexj\Documents\Research\Canadian Urban Forest Inventories - Structure and Diversity\Python Scripts and Datasets\(1) Master Dataset.csv', low_memory=False)
 species_clean_df = pd.read_csv(r'C:\Users\alexj\Documents\Research\Canadian Urban Forest Inventories - Structure and Diversity\Python Scripts and Datasets\Non-Inventory Datasets\Find and Replace.csv', low_memory=False)
 
-# Clean the Botanical Name column
+## Clean the Botanical Name column
 initial_count = merged_df.shape[0]
+
+# Deal with blank (missing) species ID, then make all species names lowercase and trim spaces
+merged_df['Botanical Name'] = merged_df['Botanical Name'].replace('', pd.NA).fillna('missing')
 merged_df['Botanical Name'] = merged_df['Botanical Name'].str.lower().str.strip()
-merged_df['Botanical Name'] = merged_df['Botanical Name'].replace('', pd.NA)  # Treat empty strings as NaN
-for index, row in species_clean_df .iterrows(): # Loop through the DataFrame and apply the replacements
+
+# Use find and replace to deal with spelling mistakes in inventory datasets
+for index, row in species_clean_df.iterrows():
     find = row['Find']
     replace = row['Replace']
-    merged_df['Botanical Name'] = merged_df['Botanical Name'].str.replace(find, replace, regex=True)
-merged_df['Botanical Name'] = merged_df['Botanical Name'].fillna('Missing')   # Fill NaN with 'Missing'
-merged_df['Botanical Name'] = ( # Fix convention and textual errors in inventories
-    merged_df['Botanical Name']
-    .str.replace(r'\bundefined\b', 'spp.', regex=True)
-    .str.replace(r'\bspecies\b', 'spp.', regex=True)
-    .str.replace(r'\bxx\b', 'x', regex=True)
-    .str.replace('bedrock unsuitable', 'missing', regex=True)
-    .str.replace('broadleaf deciduous', 'missing', regex=True)
-    .str.replace('conifer evergreen', 'missing', regex=True)
-    .str.replace(r'\bother sylvatica\b', 'sylvatica spp.', regex=True)
-    .str.replace(r'\bother\b', 'missing', regex=True)
-    .str.replace(r'\bunknown spp.\b', 'missing', regex=True)
-    .str.replace(r'\bvacant spp.\b', 'missing', regex=True)
-    .str.replace('tbd', 'missing', regex=True)
-    .str.replace(r'\bmissing large\b', 'missing', regex=True)
-    .str.replace(r'\bmissing small\b', 'missing', regex=True)
-    .str.replace(r'\bmissing spp.\b', 'missing', regex=True)
-    .str.replace(r'\bstump retained\b', 'stump', regex=True)
-    .str.replace(r'\bstump species\b', 'stump', regex=True)
-    .str.replace(r'\bwildlife snag\b', 'dead', regex=True)
-    .str.replace(r'\bto be determined\b', 'missing', regex=True)
-    .str.replace('Ã—', '', regex=True)
-    .str.replace('Ã', '', regex=True)
-    .str.replace('_x000d_', '', regex=True)
-    .str.replace('â€˜', '', regex=True)
-    .str.replace('â€™', '', regex=True)
-)
-merged_df['Botanical Name'] = ( # Fix spelling errors in Botanical Names
-    merged_df['Botanical Name']
-    .str.replace(r'\bdeodora\b', 'deodara', regex=True)
-    .str.replace(r'\boccidenatlis\b', 'occidentalis', regex=True)
-    .str.replace(r'\bcercidiphylum\b', 'cercidiphyllum', regex=True)
-    .str.replace(r'\bjaponicus\b', 'japonicum', regex=True)
-    .str.replace(r'\bnutallii\b', 'nuttallii', regex=True)
-    .str.replace(r'\bnuttalli\b', 'nuttallii', regex=True)
-    .str.replace(r'\btilia tormentosa\b', 'tilia tomentosa', regex=True)
-    .str.replace(r'\btilia to be determined\b', 'tilia spp.', regex=True)
-    .str.replace(r'\bulmus to be determined\b', 'ulmus spp.', regex=True)
-    .str.replace(r'\bulmus missing\b', 'ulmus spp.', regex=True)
-    .str.replace(r'\btilia missing\b', 'tilia spp.', regex=True)
-    .str.replace('unknown spp.', 'missing', regex=True)
-    .str.replace('vacant spp.', 'missing', regex=True)
-    .str.replace(r"\bcorylus 'cornuta'\b", 'corylus cornuta', regex=True)
-    .str.replace(r'\bcorylus avellena\b', 'corylus avellana', regex=True)
-    .str.replace(r'\bcorylus contorta\b', "corylus avellana 'contorta'", regex=True)
-    .str.replace(r'\bcorylus columa\b', 'corylus colurna', regex=True)
-    .str.replace(r'\btrachycarpus fortuneii\b', 'trachycarpus fortunei', regex=True)
-    .str.replace(r'\bulmus davidii\b', 'ulmus davidiana', regex=True)
-    .str.replace(r'\bpaper bark maple\b', 'acer griseum', regex=True)
-    .str.replace(r'\broyal crown\b', 'missing', regex=True)
-    .str.replace('chamacyparis', 'chamaecyparis', regex=True)
-    .str.replace('acre', 'acer', regex=True)
-    .str.replace('acer tartaricum', 'acer tataricum', regex=True)
-    .str.replace('acer saccharam', 'acer saccharum', regex=True)
-    .str.replace('gingko', 'ginkgo', regex=True)
-    .str.replace('flowering cherry', 'Prunus spp.', regex=True)
-    .str.replace('biloga', 'biloba', regex=True)
-    .str.replace('catalpa bignoniodes', 'catalpa bignonioides', regex=True)
-    .str.replace('gymnocladus diocia', 'gymnocladus dioicus', regex=True)
-    .str.replace('gymnocladus dioca', 'gymnocladus dioicus', regex=True)
-)
 
-filtered_df = merged_df[~merged_df["Botanical Name"].isin(["dead", "stump", "shrub", "shrubs", "vine", "vines", "hedge", "missing", "unknown", "vacant"])]
+    # Ensure word boundaries are respected in the replacement to avoid partial replacements
+    pattern = r'\b' + re.escape(find) + r'\b'
+
+    merged_df['Botanical Name'] = merged_df['Botanical Name'].str.replace(pattern, replace, regex=True)
+
+# Remove any non-living trees
+filtered_df = merged_df[~merged_df["Botanical Name"].isin(["dead", "stump", "shrub", "shrubs", "vine", "vines", "hedge", "vacant"])]
+
+# Add spp. to genus-only identification
+def add_spp_if_only_genus(name):
+    if isinstance(name, str) and len(name.strip().split()) == 1:  # Trim spaces and check if there's only one word
+        return name.strip() + " spp."  # Trim spaces and add spp.
+    return name.strip()  # Trim spaces in any case
+filtered_df.loc[:, 'Botanical Name'] = filtered_df['Botanical Name'].apply(add_spp_if_only_genus) # Apply the function to the 'Botanical Name' column
 final_count = filtered_df.shape[0]
 
-def add_spp_if_only_genus(name): # Add spp. to genus-only identification
-    if isinstance(name, str) and len(name.split()) == 1:  # Check if the Botanical Name is a string and has only one word
-        return name + " spp."
-    return name
+# Remove any incorrect letters
+filtered_df['Botanical Name'] = filtered_df['Botanical Name'].str.replace("Ã—", "", regex=False)\
+    .replace("Ã", "", regex=False)\
+    .replace("_x000d_", "", regex=False)\
+    .replace("â€˜", "", regex=False)\
+    .replace("â€™", "", regex=False)\
+    .replace("'", "", regex=False)
 
-filtered_df.loc[:, 'Botanical Name'] = filtered_df['Botanical Name'].apply(add_spp_if_only_genus) # Apply the function to the 'Botanical Name' column
+# Ensure any non-living trees are removed
+filtered_df['Botanical Name'] = filtered_df['Botanical Name'].str.replace("spp. spp.", "missing", regex=False)
 
-# Clean the DBH column
+## Clean the DBH column
 filtered_df.loc[:, "DBH"] = pd.to_numeric(filtered_df["DBH"], errors='coerce')
 filtered_df.loc[filtered_df["DBH"] > 350, "DBH"] = 0
 filtered_df.loc[filtered_df["DBH"] == 0, "DBH"] = pd.NA
@@ -140,11 +100,12 @@ blank_ctuid_df_after_filling = filtered_df[filtered_df["CTUID"].isna()]
 num_instances_blank_ctuid_after_filling = blank_ctuid_df_after_filling.shape[0]
 
 # Save the updated DataFrame to the master CSV file
-filtered_df.to_csv(r'C:\Users\alexj\Documents\Research\Canadian Urban Forest Inventories - Structure and Diversity\Python Scripts and Datasets\Filtered Master Dataset.csv', index=False)
-print("Filtered Master Dataset file created successfully.")
+filtered_df.to_csv(r'C:\Users\alexj\Documents\Research\Canadian Urban Forest Inventories - Structure and Diversity\Python Scripts and Datasets\(2) Filtered Master Dataset.csv', index=False)
 
 # Print the counts
 print(f"Number of trees where CTUID is blank after filling: {num_instances_blank_ctuid_after_filling}")
 print(f"Number of trees after removing trees with missing DAUID and CTUID: {final_count_after_missing_removal}")
 print(f"Number of out-of-city trees removed: {final_count - final_count_after_missing_removal}")
 print(f"Number of dead trees, stumps, missing, etc. removed: {initial_count - final_count}")
+
+print("Filtered Master Dataset file created successfully.")
